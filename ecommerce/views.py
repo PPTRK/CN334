381a7c51d100db5494c5b7f9eb5826fa413200b2
PPTRK <phattarakorn2705@gmail.com>
2024-01-28 20:12:48 +0700

    add page

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -13,3 +13,24 @@ def item_view(request, item_id):
         "item_id": item_id   
     }
     return render(request, 'index.html',context= context_data)
+
+def item_view1(request):   
+    
+    return render(request, 'homepage.html')
+
+def item_view2(request):   
+    
+    return render(request, 'category.html')
+
+def item_view3(request):   
+    
+    return render(request, 'product.html')
+
+def item_view4(request):   
+    
+    return render(request, 'checkout.html')
+
+def item_view5(request):   
+    
+    return render(request, 'contact.html')
+
